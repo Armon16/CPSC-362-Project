45e9c4c2b01e0427975821e04aa6a393dcaff361
Armon16 <42981215+Armon16@users.noreply.github.com>
2020-04-30 16:28:13 -0700

    Add files via upload

---
+++ ABCOnlione/User.h
@@ -0,0 +1,15 @@
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+class User {
+private:
+	string username;
+	string password;
+	string email;
+	int user_level;
+};
+#endif // !USER_H
