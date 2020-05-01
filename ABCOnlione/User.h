1edf76b1f9ae754af68a263dc99b33944e580d41
Armon16 <42981215+Armon16@users.noreply.github.com>
2020-04-30 21:06:26 -0700

    Add files via upload

--- ABCOnlione/User.h
+++ ABCOnlione/User.h
@@ -11,5 +11,24 @@ private:
 	string password;
 	string email;
 	int user_level;
+public:
+	string createUser(string name, string pass, string mail, int level) {
+		username = name;
+		password = pass;
+		email = mail;
+		user_level = level;
+	}
+	string getUsername() {
+		return username;
+	}
+	string getPassword() {
+		return password;
+	}
+	string getEmail() {
+		return email;
+	}
+	int getUser_level() {
+		return user_level;
+	}
 };
 #endif // !USER_H
